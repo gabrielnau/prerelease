851723f9c3daa4891a9f2d9ede7a00a7831a3ed1
Mathieu Ravaux <mathieu.ravaux@gmail.com>
2012-05-16 19:57:49 +0200

    Fleshed out the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,34 @@ class User
 
   field :email
   field :password
+  field :login_count, :type => Integer, :default => 0
+  field :last_login_at, :type => DateTime
+  field :current_login_at, :type => DateTime
+  field :last_login_ip, :type => String
+  field :current_login_ip, :type => String
+  field :encrypted_password, :default => "", :type => String, :null => false
+  field :password_salt, :type => String
+  field :authentication_token, :type => String
+  field :reset_password_token, :type => String
+  field :reset_password_sent_at, :type => DateTime
+  field :remember_token, :type => String
+  field :remember_created_at, :type => DateTime
+  field :sign_in_count, :type => Integer, :default => 0
+  field :current_sign_in_at, :type => DateTime
+  field :last_sign_in_at, :type => DateTime
+  field :current_sign_in_ip, :type => String
+  field :last_sign_in_ip, :type => String
+  field :failed_attempts, :type => Integer, :default => 0
+  field :unlock_token, :type => String
+  field :locked_at, :type => DateTime
+  field :muted_at, :type => DateTime, :default => nil
+
+  index :authentication_token
+  index :reset_password_token
+
 
   attr_accessible :email, :password, :remember_me
-  validate_presence_of :email, :password
-
+  validates_presence_of :email, :password
+  
+  has_and_belongs_to_many :labels
 end
